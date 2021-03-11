0e7a58559eb53fbd5a68493186b3134a9b864357
daniel <tredeneo@hotmail.com>
2021-03-10 21:15:01 -0300

    Update script_install.sh

--- script_install.sh
+++ script_install.sh
@@ -5,11 +5,10 @@ sudo apt upgrade -y
 
 
 sudo apt update
-sudo apt install -y clang clang-format git make kate calibre anki flatpak snapd
+sudo apt install -y clang-format git  kate calibre anki flatpak snapd
 
 #desenvolvimento c/c++
-sudo apt install -y clang clang-tidy clang-tools
-
+sudo apt install -y clang clang-tidy clang-tools make
 
 sudo flatpak --user remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 #set -U fish_user_paths /snap/bin/ $fish_user_paths 
@@ -18,12 +17,9 @@ sudo chmod 755 /  #   ---liberar snap:w
 sudo snap install nvim --classic
 git clone https://github.com/tredeneo/configs.git -c ~/
 mkdir -p ~/.config/nvim
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 cp ~/configs/neo\ vim/~/.config/nvim/* ~/.config/nvim/
 cp ~/configs/compile_flags.txt ~/.config/
 rm -rf ~/configs/
 sudo apt install -y fish
 sudo chsh -s /usr/bin/fish $USER
 curl -L https://get.oh-my.fish | fish
-
