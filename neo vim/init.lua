797bbe586b79ce768a1fdddfdcff459287947a31
tredeneo <tredeneo@gmail.com>
2021-09-02 23:16:49 -0300

    cores mais coerente com os temas, e twilight funcionando

--- neo vim/init.lua
+++ neo vim/init.lua
@@ -21,12 +21,8 @@ vim.wo.{option}: window-local options
 -- vim.v.{name}: predefined Vim variables
 -- vim.env.{name}: environment variables
 -- 
-
---vim.cmd "let g:dart_format_on_save = 1"
 --cursor:normal-inserir-visual-comando=linha verical,substituir=linha horizontal
 vim.o.guicursor='n-i-v-c:ver100-blinkon1,r:hor100-blinkon0'
---plugins
-
 
 ---numeros relativos
 vim.wo.number = true
@@ -38,43 +34,24 @@ vim.o.incsearch = true
 vim.o.confirm = true
 vim.o.ignorecase = true
 
---Avoid showing message extra message when using completion
--- vim.cmd 'set shortmess+=c'
---sla a diferença desses 2
---set shortmess+=A
-
----cor da janela flutuante
--- vim.cmd 'highlight Pmenu ctermbg=1 ctermfg=15'
--- vim.cmd 'highlight PmenuSel ctermbg=15'
-vim.cmd "highlight visual_char_1 ctermfg=012"
-vim.cmd "highlight visual_char_2 ctermfg=160 ctermbg=242"
----parenteses coloridos
--- vim.cmd 'let g:rainbow_active = 1'
-
 ---tabs configs
-vim.cmd 'set ts=4 sw=2 et'
-vim.cmd 'let g:indent_guides_start_level=1'
-vim.cmd 'let g:indent_guides_guide_size=4'
-vim.cmd 'let g:indent_guides_enable_on_vim_startup = 1'
+vim.bo.ts = 4
+vim.bo.sw = 2
 
 vim.o.showtabline = 2
-
 ---n pedir pra salvar ao trocar de buffer
-vim.cmd "set hidden"
+vim.o.hidden = true
+vim.bo.iskeyword = '@,48-57,192-255'
+-- vim.g.hidden = true
 ---fechar quando for ultimo
-vim.cmd "let g:nvim_tree_auto_close = 1" 
+vim.g.nvim_tree_auto_close = true
 ---fechar ao abrir arquivo
-vim.cmd "let g:nvim_tree_quit_on_open = 1"
+vim.g.nvim_tree_quit_on_open = true
 ---definir telescope como mecanismo na tela inicial
-vim.cmd "let g:dashboard_default_executive ='telescope'"
---- plugin de indentação
-vim.g.indent_blankline_char_list ={'|', '¦', '┆', '┊'} 
-vim.g.indent_blankline_char_highlight_list = {"visual_char_1", "visual_char_2"}
-vim.g.indent_blankline_space_char_highlight_list = {"VisualNC","Visual"}
-vim.g.indent_blankline_show_trailing_blankline_indent = false
----excluir indentação na pagina inicial
-vim.g.indent_blankline_filetype_exclude = {'dashboard'}
-vim.g.indent_blankline_show_current_context = true
+vim.g.dashboard_default_executive = 'telescope'
+
+
+vim.cmd [[autocmd BufEnter * :TwilightEnable]]
 ---formatador
 vim.api.nvim_exec([[
 augroup FormatAutogroup
@@ -82,3 +59,5 @@ augroup FormatAutogroup
   autocmd BufWritePost *.c,*.py,*.rs FormatWrite
 augroup END
 ]], true)
+
+
--- neo vim/lua/requires.lua
+++ neo vim/lua/requires.lua
@@ -45,6 +45,8 @@ require'lspconfig'.clangd.setup{
             "--header-insertion=iwyu"}
 }
 
+require'lspconfig'.clojure_lsp.setup{}
+
 ----- flutter
 require("flutter-tools").setup{
   widget_guides = {
@@ -123,8 +125,8 @@ require("which-key").setup{}
 
 ---movimentação suave
 require('neoscroll').setup({
--- mappings = {'<C-u>', '<C-d>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'}
-mappings = {'<C-y>', '<C-e>', 'zt', 'zz', 'zb'}
+mappings = {'<C-u>', '<C-d>', '<C-y>', '<C-e>', 'zt', 'zz', 'zb'}
+-- mappings = {'<C-y>', '<C-e>', 'zt', 'zz', 'zb'}
 })
 
 ---janela de erros
@@ -157,6 +159,29 @@ require 'lspsaga'.init_lsp_saga({
   infor_sign = "I",
 })
 
+---cor da janela flutuante
+vim.cmd [[highlight IndentBlanklineIndent1 guifg=#ff0000 blend=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guifg=#00ff00 blend=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent3 guifg=#0000ff blend=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent4 guifg=#ffff00 blend=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent1 guibg=#1f1f1f blend=nocombine]]
+vim.cmd [[highlight IndentBlanklineIndent2 guibg=#1a1a1a blend=nocombine]]
+require("indent_blankline").setup{
+  -- char_list ={'|', '¦', '┆', '┊'},
+  char_highlight_list = {--"visual_char_1", "visual_char_2"},
+        "IndentBlanklineIndent1",
+        "IndentBlanklineIndent2",
+        "IndentBlanklineIndent3",
+        "IndentBlanklineIndent4",
+      },
+  show_trailing_blankline_indent = true,
+  space_char_highlight_list = {
+    "Visual","VisualNC"
+  },
+  filetype_exclude = {'dashboard'},
+  show_current_context = true 
+
+}
 
 ---adicionar/remove/substituir caractes em volta
 require"surround".setup{}
