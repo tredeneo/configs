07084b876641c38f8c10b47b4a6bf5f99e5c7fb2
tredeneo <tredeneo@hotmail.com>
2022-06-06 13:06:48 -0300

    retornando arquivos de programas individuais

--- doom emacs/init.el
+++ doom emacs/init.el
@@ -17,39 +17,25 @@
        ivy               ; a search engine for love and life
 
        :ui
-       ;;deft              ; notational velocity for Emacs
        doom             
        doom-dashboard    
        doom-quit         
-       ;;fill-column       
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
        indent-guides     
        ligatures         ; ligatures and symbols to make your code pretty again
-       ;;minimap         
        (modeline +light)          ; snazzy, Atom-inspired modeline, plus API
-       ;;nav-flash         ; blink cursor line after big motions
        neotree           ; a project drawer, like NERDTree for vim
        ophints           ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       ;;tabs            
        vi-tilde-fringe   ; fringe tildes to mark beyond EOB
-       ;;window-select     ; visually switch windows
        workspaces        ; tab emulation, persistence & separate workspaces
-       ;;zen
        
        :editor
        (evil +everywhere)
        file-templates    ; auto-snippets for empty files
        fold              ; (nigh) universal code folding
        (format +onsave)  
-       ;;god               ; run Emacs commands without modifier keys
-       ;;multiple-cursors  ; editing in many places at once
-       ;;objed             ; text object editing for the innocent
-       ;;parinfer          ; turn lisp into python, sort of
-       ;;rotate-text       ; cycle region at point between text candidates
        snippets          
-       ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
        dired             ; making dired pretty [functional]
@@ -60,9 +46,6 @@
        syntax             
        
        :tools
-       ;;ansible
-       ;;direnv
-       ;;editorconfig      ; let someone else argue about tabs vs spaces
        (eval +overlay)     ; run code, run (also, repls)
        lookup              ; navigate your code and its documentation
        lsp
