9a66660560c5048ce560bf16fb513c32f87b024c
tredeneo <tredeneo@hotmail.com>
2024-03-19 01:20:08 -0300

    update nixos configuration

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -2,40 +2,86 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, lib, ... }:
+{ config, pkgs,lib, ... }:
 
 {
-  imports = [ # Include the results of the hardware scan.
-    ./hardware-configuration.nix
-  ];
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.binfmt.registrations.appimage = {
+    wrapInterpreterInShell = false;
+    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+    recognitionType = "magic";
+    offset = 0;
+    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+    magicOrExtension = ''\x7fELF....AI\x02'';
+  };
 
   # Setup keyfile
-  boot.initrd.secrets = { "/crypto_keyfile.bin" = null; };
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+
+
+  # systemd.services.NetworkManager-wait-online = {
+    # enable=false;
+    # serviceConfig = {
+    #   ExecStart = [ "" "${pkgs.networkmanager}/bin/nm-online -q" ];
+    # };
+  # };
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
 
   boot.kernelPackages = pkgs.linuxPackages_zen;
+  services.system76-scheduler.enable = true;
 
   # Enable swap on luks
-  boot.initrd.luks.devices."luks-f2c35767-a383-4e3e-8473-34be359147ff".device =
-    "/dev/disk/by-uuid/f2c35767-a383-4e3e-8473-34be359147ff";
-  boot.initrd.luks.devices."luks-f2c35767-a383-4e3e-8473-34be359147ff".keyFile =
-    "/crypto_keyfile.bin";
-
-  boot.loader = {
-    # Bootloader.
-    systemd-boot.enable = true;
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
+  ##boot.initrd.luks.devices."luks-f2c35767-a383-4e3e-8473-34be359147ff".device =
+  ##  "/dev/disk/by-uuid/f2c35767-a383-4e3e-8473-34be359147ff";
+  ##boot.initrd.luks.devices."luks-f2c35767-a383-4e3e-8473-34be359147ff".keyFile =
+  # # "/crypto_keyfile.bin";
+  hardware = {
+    logitech.wireless = {
+      enableGraphical= true; # unifying receiver
+      enable = true;# unifying receiver
+    };
+    bluetooth = {
+      enable = true;
+      
     };
   };
-  boot.plymouth = {
-    enable = true;
-    theme = "breeze";
-    themePackages = with pkgs; [ libsForQt5.breeze-plymouth ];
-  };
+  boot.supportedFilesystems = ["ntfs"];
+  #boot.loader = {
+    # Bootloader.
+    ##systemd-boot.enable = true;
+   # grub = {
+    #  enable = true;
+     # device = "/dev/sdb";
+      #useOSProber = true;
+#    };
+
+   # #efi = {
+    # # canTouchEfiVariables = true;
+    #  #efiSysMountPoint = "/boot/efi";
+    #};
+#  };
+  # boot.plymouth = {
+    # enable = true;
+    # theme = "breeze";
+    # themePackages = with pkgs; [ libsForQt5.breeze-plymouth ];
+  # };
 
   zramSwap = {
-    enable = false;
+    enable = true;
   };
   networking = {
     firewall = {
@@ -65,7 +111,6 @@
       enabled = "fcitx5";
       #ibus.engines = with pkgs.ibus-engines; [rime hangul ];
       fcitx5.addons = with pkgs; [ fcitx5-rime ];
-      fcitx5.enableRimeData = true;
     };
     defaultLocale = "pt_BR.utf8";
   };
@@ -81,25 +126,41 @@
 
   # Configure console keymap
   programs = {
+    fish.enable = true;
     xwayland.enable = true;
     sway.enable = false;
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # mtr.enable = true;
-    # gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
+    dconf.enable=true;
+    nano.enable=false;
+    virt-manager.enable=true;
+    droidcam.enable=true;
+    corectrl = {
+      enable= true;
+      gpuOverclock= {
+        enable=true;
+        ppfeaturemask = "0xffffffff";
+      };
+
+    };
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # mtr.enable = true;
+  # gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
   };
 
   fonts = {
-    enableDefaultFonts = lib.mkForce false;# true -> error
-    fontDir.enable = true;
-    #flatpak --user override --filesystem=$HOME/.local/share/fonts
-    # flatpak --user override --filesystem=$HOME/.icons
-    fonts = [
+    enableDefaultPackages= lib.mkForce false;# true -> error
+    fontDir.enable = true;# flatpak font user 
+    # $ flatpak --user override --filesystem=$HOME/.local/share/fonts
+    # $ flatpak --user override --filesystem=$HOME/.icons    
+
+    packages= [
       (pkgs.nerdfonts.override { fonts = [ "CodeNewRoman" "SpaceMono" ]; })
-      pkgs.source-han-mono
+      pkgs.source-han-sans#-vf-ttf
+      pkgs.source-han-serif
+      # pkgs.i-dot-ming
       pkgs.twitter-color-emoji # Twitter Color Emoji
       pkgs.symbola # tudo q é simbolo de escritas exotivas
     ];
@@ -123,11 +184,23 @@
 
     };
   };
-  security.rtkit.enable = true;
+  security = {
+    rtkit.enable = true;# nixos wiki pipewire
+    sudo-rs.enable= true;
+  };
 
   services = {
-    cinnamon.apps.enable = false;
-    fwupd.enable = true;
+
+    mysql = {
+    enable = false;
+    package= pkgs.mariadb;
+    };
+
+    power-profiles-daemon.enable=true;# enable power profile selection
+    fwupd= {# sudo fwupdmgr update
+      enable = true;
+      #extraRemotes = ["lvfs" "vendor" "vendor-directory"]
+    };
     xserver = {
       enable = true;
       layout = "br";
@@ -137,43 +210,40 @@
       };
       desktopManager = {
         plasma5.enable = true;
-        plasma5.excludePackages = [ pkgs.libsForQt5.kwayland pkgs.libsForQt5.kwayland-integration];
-        cinnamon.enable = false;
-        lxqt.enable = true;
-        xfce = {
-          enable = true;
-          noDesktop = false;
-          enableXfwm = false;
-        };
       };
       windowManager = {
-        qtile.enable = true;
+        qtile.enable =false;
         herbstluftwm.enable = false;
-        leftwm.enable = true;
+        leftwm.enable = false;
       };
 
     };
     xrdp = {
-      enable = true;
+      enable = false; # not work on wayland
       defaultWindowManager = "startplasma-x11";
     };
     openssh = {
-      enable=true;
-      passwordAuthentication = false;
-      kbdInteractiveAuthentication = false;
+      enable=false;
+      settings ={
+
+        PasswordAuthentication = true;
+        KbdInteractiveAuthentication = true;
+      };
     };
     flatpak.enable = true;
-    # Enable CUPS to print documents.
-    printing = {
 
+    printing = {
+    # Enable CUPS to print documents.
       enable = true;
       drivers = [ pkgs.gutenprint ];
     };
     avahi = {
       enable = true;
-      openFirewall = true;
       nssmdns = true;
+      # wifi(IPP) printers
+      #openFirewall = true;
     };
+    tailscale.enable = false;
     pipewire = {
       enable = true;
       alsa.enable = true;
@@ -186,30 +256,28 @@
     };
     clamav = {
       # $ sudo freshclam
-      daemon.enable = false;
-      updater.enable = false;
+      daemon.enable = true;
+      updater.enable = true;
     };
-    auto-cpufreq.enable = true;
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # openssh.enable = true;
 
     # Enable touchpad support (enabled default in most desktopManager).
-    xserver.libinput.enable = true;
+    #xserver.libinput.enable = true;
   };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.tmp = {
     isNormalUser = true;
+    description=  "tmp temporario";
 
-    extraGroups = [ "scanner" "lp" "networkmanager"];   
+    extraGroups = [ "scanner" "lp" "networkmanager"];
     };
+
+  users.extraGroups.vboxusers.members = [ "dnl" ];
   users.users.dnl = {
     isNormalUser = true;
-    shell = pkgs.fish;
-    description = "dnl";
-    extraGroups = [ "lxd" "scanner" "lp" "networkmanager" "wheel" ];
+    # shell = pkgs.fish;
+    description = "dnl padrão";
+    extraGroups = [ "scanner" "lp" "networkmanager" "wheel" "libvirtd"];
     packages = with pkgs; [ distrobox ];
   };
   nixpkgs.config.allowUnfree = true;
@@ -222,66 +290,103 @@
       VISUAL = "hx";
       SUDO_EDITOR = "hx";
     };
+
+
     defaultPackages = lib.mkForce [ pkgs.helix ];
     systemPackages = with pkgs; [
 
-      xfce.xfce4-notifyd
-      xfce.xfce4-settings      
-      xfce.xfce4-timer-plugin
-      xfce.xfce4-volumed-pulse
-      xfce.xfce4-power-manager
-      xfce.xfce4-weather-plugin
-      xfce.xfce4-sensors-plugin
-      xfce.xfce4-cpufreq-plugin
-      xfce.xfce4-clipman-plugin
-      xfce.xfce4-battery-plugin
-      xfce.xfce4-datetime-plugin
-      xfce.xfce4-cpugraph-plugin
-      xfce.xfce4-pulseaudio-plugin
-      #xfce.xfce4-hardware-monitor-plugin #broken
-      xfce.xfce4-systemload-plugin
+      xfce.thunar
+      xfce.thunar-volman
+      xfce.thunar-archive-plugin
+      xfce.thunar-media-tags-plugin
 
-    
+
+      flameshot # screenshot , flatpak
+
+      git
+      gh
+
+
+
+      cargo-cross # rust cross-platforma target build
+      qemu_kvm
+      qutebrowser
+      devbox #best container system for dev
+
+      zathura #pdf reader with vim motions
+      deja-dup # simple file backup system
+
+      quickemu #simple qemu vm 
+
+      xwaylandvideobridge #fix discord screen share on wayland
+      
       wlr-randr
       brise # rime schemes
-      kate
-      xclip
-      wl-clipboard
-      steam-run
-      libsForQt5.discover
-      libsForQt5.bismuth
+      kate # text editor
+      xclip # copy/cur terminal xorg
+      wl-clipboard # copy cut terminal wayland
+      steam-run # run normal binary linux apps
+      gnome.gnome-software
       libsForQt5.powerdevil
       libsForQt5.ark
-      unp
-      firefox-wayland
+      unp # unpack zip,tar,gz...
+      firefox-esr
+      solaar # logitech keyboard change FN key
+      # logitech-udev-rules # need for solaar
+
+      
+
+      kinfocenter
+      hardinfo
+      pciutils
+
+      rocmPackages.clr
+      rocmPackages.rocminfo
+      rocmPackages.rocm-smi
+
+      virtio-win
+
     ];
   };
 
   virtualisation = {
-    oci-containers = {
-      backend = "podman";
-      containers = {
-        container-name = {
-          image = "container-image";
-          autoStart = true;
-          ports = [ "127.0.0.1:1234:1234" ];
-        };
-      };
+    #oci-containers = {
+    #  backend = "podman";
+    #  containers = {
+    #    container-name = {
+    #      image = "container-image";
+    #      autoStart = true;
+    #      ports = [ "127.0.0.1:1234" ];
+    #    };
+    #  };
+    #};
+    waydroid.enable=false;
+    virtualbox.host = {
+      enable = false;
+      enableExtensionPack = false;
+
     };
-    lxd.enable=true;
-    lxd.recommendedSysctlSettings = true;
+    #lxd.recommendedSysctlSettings = true;
+    libvirtd.enable = true;
     podman = {
       enable = true;
       # Create a `docker` alias for podman, to use it as a drop-in replacement
       dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
-      defaultNetwork.dnsname.enable = true;
+      defaultNetwork.settings.dns_enable = true;
 
-      networkSocket.enable=false;
+     # networkSocket.enable=false;
     };
   };
-
+  system.copySystemConfiguration=true; #  /run/current-system/configuration.nix
+  # system.userActivationScripts = {
+    # flatpak_local_font.text = ''
+    # if [[ ! -h "$HOME/.local/share/fonts" ]]; then
+    #   ln -s /run/current-system/sw/share/X11/fonts ~/.local/share/fonts
+    # fi
+    # '';
+  # };
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
